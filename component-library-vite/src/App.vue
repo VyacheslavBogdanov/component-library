f6bb7bda38b02815defce1eda42d09a7f5f37411
Михаил Константинов <mihailkonstantinov@Mihails-MacBook-Pro.local>
2024-11-18 18:21:24 +0400

    SearchFix

--- component-library-vite/src/App.vue
+++ component-library-vite/src/App.vue
@@ -15,14 +15,14 @@
 <script setup lang="ts">
 import TestComponent from '@/components/TestComponent/TestComponent.vue';
 
-// import Search from '@/components/Search/Search.vue';
-// import DropdownWithSearch from '@/components/DropdownWithSearch/DropdownWithSearch.vue';
-// import CheckboxFilter from '@/components/CheckboxFilter/CheckboxFilter.vue';
-// import RadioButtonFilter from '@/components/RadioButtonFilter/RadioButtonFilter.vue';
-// import Chips from '@/components/Chips/Chips.vue';
-// import ChipsFilter from '@/components/ChipsFilter/ChipsFilter.vue';
-// import DatePicker from '@/components/DatePicker/DatePicker.vue';
-// import Informer from '@/components/Informer/Informer.vue';
+import Search from '@/components/Search/Search.vue';
+import DropdownWithSearch from '@/components/DropdownWithSearch/DropdownWithSearch.vue';
+import CheckboxFilter from '@/components/CheckboxFilter/CheckboxFilter.vue';
+import RadioButtonFilter from '@/components/RadioButtonFilter/RadioButtonFilter.vue';
+import Chips from '@/components/Chips/Chips.vue';
+import ChipsFilter from '@/components/ChipsFilter/ChipsFilter.vue';
+import DatePicker from '@/components/DatePicker/DatePicker.vue';
+import Informer from '@/components/Informer/Informer.vue';
 </script>
 
 <style lang="scss" scoped></style>
