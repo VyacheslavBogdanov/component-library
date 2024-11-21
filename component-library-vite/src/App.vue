0ad3ad169326cb4916ec731f91c0703654ea2a0e
Vyacheslav Bogdanov <sbhlrsa@gmail.com>
2024-11-21 15:18:28 +0400

    Date picker and Search fix

--- component-library-vite/src/App.vue
+++ component-library-vite/src/App.vue
@@ -1,22 +1,17 @@
 <template>
 	<div id="app">
 		<Search />
+		<DatePicker />
 		<!-- <DropdownWithSearch />
 		<CheckboxFilter />
 		<RadioButtonFilter />
 		<Chips />
-
 		<ChipsFilter /> -->
-		<DatePicker />
 		<!-- <Informer /> -->
-		<!-- <TestComponent /> -->
-
 	</div>
 </template>
 
 <script setup lang="ts">
-// import TestComponent from '@/components/TestComponent/TestComponent.vue';
-
 import Search from '@/components/Search/Search/SearchForm.vue';
 // import DropdownWithSearch from '@/components/DropdownWithSearch/DropdownWithSearch.vue';
 // import CheckboxFilter from '@/components/CheckboxFilter/CheckboxFilter.vue';
