a553a402d8a84d9b467d79f976e2e26c971144c0
Vyacheslav Bogdanov <sbhlrsa@gmail.com>
2024-11-26 12:59:06 +0400

    redesigned the template and styles according to БЭМ

--- component-library-vite/src/components/Chips/Chips.vue
+++ component-library-vite/src/components/Chips/Chips.vue
@@ -1,79 +1,93 @@
 <template>
-	<h1>Chips</h1>
-	<div class="filter-container" ref="filterContainer">
-		<label
-			:class="{
-				'filter-container__label': true,
-				'filter-container__label--active': isDropdownVisible,
-			}"
-			>Исполнитель</label
-		>
-		<div
-			:class="{
-				'filter-container--iconPositionTop': isDropdownVisible,
-				'filter-container--iconPositionDown': !isDropdownVisible,
-			}"
-		>
-			⌃
-		</div>
-		<input
-			readonly
-			type="text"
-			v-model="displayText"
-			@focus="isDropdownVisible = true"
-			:class="{
-				'filter-container__input': true,
-				'filter-container__input--active': isDropdownVisible,
-			}"
-		/>
-		<div v-if="isDropdownVisible" class="dropdown" @mousedown="handleDropdownClick">
-			<div class="search">
-				<input
-					class="search__input"
-					type="text"
-					v-model="searchQuery"
-					placeholder="Поиск"
-					@focus="isDropdownVisible = true"
-				/>
-				<span class="search__icon"></span>
+	<div class="Chips">
+		<h1>Chips</h1>
+		<div class="filter-container" ref="filterContainer">
+			<label
+				:class="{
+					'filter-container__label': true,
+					'filter-container__label--active': isDropdownVisible,
+				}"
+				>Исполнитель</label
+			>
+			<div
+				:class="{
+					'filter-container--iconPositionTop': isDropdownVisible,
+					'filter-container--iconPositionDown': !isDropdownVisible,
+				}"
+			>
+				⌃
 			</div>
-			<ul class="dropdown-list">
-				<li v-if="itemsToDisplay.length > 0">
-					<label>
-						<input type="checkbox" v-model="selectAll" @change="handleSelectAll" />
-						<span>Все</span>
-					</label>
-				</li>
-				<li v-for="(item, index) in itemsToDisplay" :key="index">
-					<label>
-						<input
-							type="checkbox"
-							v-model="checkedItems"
-							:value="item"
-							@change="updateDisplayText"
-						/>
-						<span v-html="highlightMatch(item)"></span>
-					</label>
-				</li>
-			</ul>
-			<div v-if="noResultsFound" class="no-results">Результаты не найдены</div>
+			<input
+				readonly
+				type="text"
+				v-model="displayText"
+				@focus="isDropdownVisible = true"
+				:class="{
+					'filter-container__input': true,
+					'filter-container__input--active': isDropdownVisible,
+				}"
+			/>
+			<div v-if="isDropdownVisible" class="dropdown" @mousedown="handleDropdownClick">
+				<!-- Разобраться с блоком .search-->
+				<div class="search-wrapper" v-if="showSearch">
+					<input
+						class="search"
+						type="text"
+						v-model="searchQuery"
+						placeholder="Поиск"
+						@focus="isDropdownVisible = true"
+					/>
+					<span class="searchicon"></span>
+				</div>
+				<!-- Разобраться с блоком .search-->
+				<ul class="dropdown-list">
+					<li v-if="itemsToDisplay.length > 0" class="dropdown-list__item">
+						<label class="dropdown-list__label">
+							<input
+								class="dropdown-list__checkbox"
+								type="checkbox"
+								v-model="selectAll"
+								@change="handleSelectAll"
+							/>
+							<span class="dropdown-list__text">Все</span>
+						</label>
+					</li>
+					<li
+						v-for="(item, index) in itemsToDisplay"
+						:key="index"
+						class="dropdown-list__item"
+					>
+						<label class="dropdown-list__label">
+							<input
+								class="dropdown-list__checkbox"
+								type="checkbox"
+								v-model="checkedItems"
+								:value="item"
+								@change="updateDisplayText"
+							/>
+							<span class="dropdown-list__text" v-html="highlightMatch(item)"></span>
+						</label>
+					</li>
+				</ul>
+				<div v-if="noResultsFound" class="no-results">Результаты не найдены</div>
+			</div>
 		</div>
-	</div>
-	<div v-if="!isDropdownVisible" class="chips-container">
-		<div
-			v-for="(chip, index) in checkedItems"
-			:key="index"
-			class="chip"
-			@mouseenter="showTooltip(chip, $event)"
-			@mousemove="moveTooltip($event)"
-			@mouseleave="hideTooltip"
-		>
-			<span class="chip-content">{{ chip }}</span>
-			<button class="delete-chip" @click="removeChip(chip)">
-				<div class="delete-chip-icon">✕</div>
-			</button>
+		<div v-if="!isDropdownVisible" class="chips-container">
+			<div
+				v-for="(chip, index) in checkedItems"
+				:key="index"
+				class="chips-container__chip"
+				@mouseenter="showTooltip(chip, $event)"
+				@mousemove="moveTooltip($event)"
+				@mouseleave="hideTooltip"
+			>
+				<span class="chips-container__chip-content">{{ chip }}</span>
+				<button class="chips-container__delete-chip" @click="removeChip(chip)">
+					<div class="chips-container__delete-chip-icon">✕</div>
+				</button>
+			</div>
+			<span v-if="tooltipText" :style="tooltipStyle" class="tooltip">{{ tooltipText }}</span>
 		</div>
-		<span v-if="tooltipText" :style="tooltipStyle" class="tooltip">{{ tooltipText }}</span>
 	</div>
 </template>
 
@@ -309,6 +323,7 @@ $chip-border-radius: 16px;
 		font-weight: lighter;
 		color: $icon-color-active;
 		cursor: $cursor;
+		pointer-events: none;
 	}
 
 	&--iconPositionDown {
@@ -320,6 +335,7 @@ $chip-border-radius: 16px;
 		font-weight: lighter;
 		color: $icon-color;
 		cursor: $cursor;
+		pointer-events: none;
 	}
 	&__input {
 		width: 100%;
@@ -360,6 +376,8 @@ $chip-border-radius: 16px;
 	display: flex;
 	flex-direction: column;
 
+	// Разобраться с блоком .search
+
 	.search-wrapper {
 		position: relative;
 		width: 95%;
@@ -367,68 +385,64 @@ $chip-border-radius: 16px;
 	}
 
 	.search {
-		position: relative;
-		width: 95%;
-		margin: 7px 0 0 10px;
+		width: 100%;
+		height: $height-input;
+		border: 1.5px solid $border-color;
+		border-radius: 8px;
+		outline: none;
+		padding-right: 40px;
+		padding-left: 10px;
+		box-sizing: border-box;
+		font-size: $font-size;
 
-		&__input {
-			width: 100%;
-			height: $height-input;
-			border: 1.5px solid $border-color;
-			border-radius: 8px;
-			outline: none;
-			padding-right: 40px;
-			padding-left: 10px;
-			box-sizing: border-box;
-			font-size: $font-size;
+		&:focus {
+			border-color: $focus-color;
 
-			&:focus {
-				border-color: $focus-color;
-
-				+ .search__icon {
-					&::before {
-						border-color: $focus-color;
-					}
-					&::after {
-						background: $focus-color;
-					}
+			~ .searchicon {
+				&::before {
+					border-color: $focus-color;
+				}
+				&::after {
+					background: $focus-color;
 				}
 			}
 		}
+	}
 
-		&__icon {
+	.searchicon {
+		position: absolute;
+		right: 10px;
+		top: 50%;
+		transform: translateY(-50%);
+		width: 30px;
+		height: 30px;
+		display: grid;
+		place-items: center;
+		pointer-events: none;
+
+		&::before {
+			content: '';
+			width: 9.5px;
+			height: 9.5px;
+			border: 1.5px solid $border-color;
+			border-radius: 50%;
+			transition: border-color 0.2s;
 			position: absolute;
-			right: 10px;
-			top: 50%;
-			transform: translateY(-50%);
-			width: 30px;
-			height: 30px;
-			display: grid;
-			place-items: center;
-			pointer-events: none;
+			transform: translate(-2px, -2px);
+		}
 
-			&::before {
-				content: '';
-				width: 9.5px;
-				height: 9.5px;
-				border: 1.5px solid $border-color;
-				border-radius: 50%;
-				transition: border-color 0.2s;
-				position: absolute;
-				transform: translate(-2px, -2px);
-			}
-
-			&::after {
-				content: '';
-				position: absolute;
-				width: 1.5px;
-				height: 9.5px;
-				background: $border-color;
-				transition: border-color 0.2s;
-				transform: rotate(-45deg) translate(-0px, 7px);
-			}
+		&::after {
+			content: '';
+			position: absolute;
+			width: 1.5px;
+			height: 9.5px;
+			background: $border-color;
+			transition: border-color 0.2s;
+			transform: rotate(-45deg) translate(-0px, 7px);
 		}
 	}
+
+	// Разобраться с блоком .search
 
 	.dropdown-list {
 		list-style: none;
@@ -436,38 +450,38 @@ $chip-border-radius: 16px;
 		margin: 0;
 		width: 100%;
 
-		li {
+		&__item {
+			cursor: $cursor;
 			padding: 8px;
 			margin: 0;
-			cursor: $cursor;
 			text-align: start;
 			position: relative;
 
 			&:hover {
 				background-color: $hover-background;
 			}
-
-			label {
-				cursor: $cursor;
-
-				input {
-					cursor: $cursor;
-				}
-			}
 		}
 
-		b {
-			font-weight: bold;
+		&__label {
+			cursor: $cursor;
 		}
-	}
 
-	.no-results {
-		color: $text-color;
-		position: relative;
-		margin: auto;
-		font-size: $font-size;
-		font-family: $font-allelement;
+		&__checkbox {
+			cursor: $cursor;
+		}
+
+		// b {
+		// 	font-weight: bold;
+		// }
 	}
+}
+
+.no-results {
+	color: $text-color;
+	position: relative;
+	margin: auto;
+	font-size: $font-size;
+	font-family: $font-allelement;
 }
 
 .chips-container {
@@ -479,7 +493,7 @@ $chip-border-radius: 16px;
 	gap: 8px;
 	margin: 12px 0 0 16px;
 
-	.chip {
+	&__chip {
 		position: relative;
 		display: flex;
 		align-items: center;
@@ -502,46 +516,46 @@ $chip-border-radius: 16px;
 		&:hover .tooltip {
 			display: block;
 		}
-
-		.chip-content {
-			user-select: none;
-			overflow: hidden;
-			text-overflow: ellipsis;
-		}
-
-		.delete-chip {
-			background: none;
-			border: none;
-			color: $icon-color;
-			font-size: 9px;
-			font-weight: bold;
-			cursor: pointer;
-			margin-left: 8px;
-
-			.delete-chip-icon {
-				height: 14px;
-				width: 14px;
-				border: none;
-				border-radius: 50%;
-				background-color: rgb(88, 85, 85);
-				color: #e0e0e0;
-				display: flex;
-				align-items: center;
-				justify-content: center;
-			}
-		}
 	}
 
-	.tooltip {
-		position: absolute;
-		background-color: rgb(21, 18, 18);
-		color: rgb(224, 211, 211);
-		font-size: 12px;
-		padding: 4px 8px;
-		border-radius: 4px;
-		white-space: nowrap;
-		z-index: 1;
-		pointer-events: none;
+	&__chip-content {
+		user-select: none;
+		overflow: hidden;
+		text-overflow: ellipsis;
 	}
+
+	&__delete-chip {
+		background: none;
+		border: none;
+		color: $icon-color;
+		font-size: 9px;
+		font-weight: bold;
+		cursor: pointer;
+		margin-left: 8px;
+	}
+
+	&__delete-chip-icon {
+		height: 14px;
+		width: 14px;
+		border: none;
+		border-radius: 50%;
+		background-color: rgb(88, 85, 85);
+		color: #e0e0e0;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+}
+
+.tooltip {
+	position: absolute;
+	background-color: rgb(21, 18, 18);
+	color: rgb(224, 211, 211);
+	font-size: 12px;
+	padding: 4px 8px;
+	border-radius: 4px;
+	white-space: nowrap;
+	z-index: 1;
+	pointer-events: none;
 }
 </style>
