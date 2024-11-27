11085b3e68197d19b7eeb2724290518fab444fdc
Vyacheslav Bogdanov <sbhlrsa@gmail.com>
2024-11-27 15:00:09 +0400

    chips fix in process

--- component-library-vite/src/components/Chips/Dropdown/Dropdown.vue
+++ component-library-vite/src/components/Chips/Dropdown/Dropdown.vue
@@ -1,6 +1,6 @@
-<template>
+<!-- <template>
 	<div v-if="isDropdownVisible" class="dropdown" @mousedown="handleDropdownClick">
-		<!-- Разобраться с блоком .search-->
+
 		<div class="search-wrapper" v-if="showSearch">
 			<input
 				class="search"
@@ -12,7 +12,7 @@
 			/>
 			<span class="searchicon"></span>
 		</div>
-		<!-- Разобраться с блоком .search-->
+
 		<ul class="dropdown-list">
 			<li v-if="itemsToDisplay.length > 0" class="dropdown-list__item">
 				<label class="dropdown-list__label">
@@ -60,8 +60,6 @@ const props = defineProps<{
 }>();
 
 const emit = defineEmits<{
-	// (event: 'ToggleHeaderDate', type: 'month' | 'year', route: 'prev' | 'next'): void;
-	// (event: 'changeSelectedMonth', value: number): void;
 	(event: 'showDropdown', value: string): void;
 	(event: 'update:modelValue', value: string): void;
 }>();
@@ -79,6 +77,103 @@ const itemsToDisplay = computed(() => {
 });
 
 const showSearch = computed(() => props.items.length > 10);
+</script>
+ -->
+
+<template>
+	<div v-if="isDropdownVisible" class="dropdown" @mousedown="handleDropdownClick">
+		<div class="search-wrapper" v-if="showSearch">
+			<input
+				class="search"
+				type="text"
+				placeholder="Поиск"
+				:value="modelValue"
+				@input="handleInput"
+				@focus="emit('showDropdown', true)"
+			/>
+			<span class="searchicon"></span>
+		</div>
+		<ul class="dropdown-list">
+			<li v-if="itemsToDisplay.length > 0" class="dropdown-list__item">
+				<label class="dropdown-list__label">
+					<input
+						class="dropdown-list__checkbox"
+						type="checkbox"
+						:checked="selectAll"
+						@change="emit('update:selectAll', !selectAll)"
+					/>
+					<span class="dropdown-list__text">Все</span>
+				</label>
+			</li>
+			<li v-for="(item, index) in itemsToDisplay" :key="index" class="dropdown-list__item">
+				<label class="dropdown-list__label">
+					<input
+						class="dropdown-list__checkbox"
+						type="checkbox"
+						:checked="checkedItems.includes(item)"
+						@change="toggleItem(item)"
+					/>
+					<span class="dropdown-list__text" v-html="highlightMatch(item)"></span>
+				</label>
+			</li>
+		</ul>
+		<div v-if="noResultsFound" class="no-results">Результаты не найдены</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import { handleDropdownClick } from '../utils/utils';
+
+const props = defineProps<{
+	modelValue: string;
+	isDropdownVisible: boolean;
+	items: string[];
+	checkedItems: string[];
+	searchQuery: string;
+	filteredList: string[];
+	selectAll: boolean;
+	highlightMatch: any;
+	noResultsFound: boolean;
+}>();
+
+const emit = defineEmits<{
+	(event: 'showDropdown', value: boolean): void;
+	(event: 'update:modelValue', value: string): void;
+	(event: 'update:selectAll', value: boolean): void;
+	(event: 'update:checkedItems', value: string[]): void;
+}>();
+
+console.log('ITEMS', props.items);
+
+const itemsToDisplay = computed(() => {
+	if (props.filteredList.length <= 10) return props.filteredList;
+
+	const selectedItems = props.filteredList
+		.filter((item) => props.checkedItems.includes(item))
+		.sort((a, b) => a.localeCompare(b));
+
+	const unselectedItems = props.filteredList.filter((item) => !props.checkedItems.includes(item));
+
+	return [...selectedItems, ...unselectedItems];
+});
+
+const showSearch = computed(() => props.items.length > 10);
+
+const toggleItem = (item: string) => {
+	const newCheckedItems = props.checkedItems.includes(item)
+		? props.checkedItems.filter((checkedItem) => checkedItem !== item)
+		: [...props.checkedItems, item];
+
+	emit('update:checkedItems', newCheckedItems);
+};
+
+const handleInput = (event: Event) => {
+	const target = event.target as HTMLInputElement | null;
+	if (target) {
+		emit('update:modelValue', target.value);
+	}
+};
 </script>
 
 <style lang="scss" scoped>
